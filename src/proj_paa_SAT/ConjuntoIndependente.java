5c4c1f6cfe5e1d17018b0afbbd2478b68605edd7
iBella <isabella.isaacfernandes@gmail.com>
2016-03-12 09:44:12 -0300

    antes dos testes

--- src/proj_paa_SAT/ConjuntoIndependente.java
+++ src/proj_paa_SAT/ConjuntoIndependente.java
@@ -5,39 +5,22 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+
+//Usado = 1
+//Nao usado = -1
+//Disponivel = 0
 
 public class ConjuntoIndependente {
-	
+
 	private int nVertice;
 	private ArrayList<ArrayList<Integer>> grafo = new ArrayList<>();
 	private ArrayList<Integer> vGraus = new ArrayList<>();
-	private ArrayList<Integer> vPertinencia = new ArrayList<>();
-	private ArrayList<Integer> sGulosa;
-	
-	private ArrayList<String> sInicial = new ArrayList<>();
-	private ArrayList<String> vMelhor = new ArrayList<>();
-	private ArrayList<String> vPertBB = new ArrayList<>();
-	private int tamMelhor;
+	private int qtVerticeSolucao;
+	private ArrayList<Integer> solucao = new ArrayList<>();
+	private ArrayList<Integer> inicial = new ArrayList<>();
 	
 	public static void main(String[] args) {
-		ConjuntoIndependente c = new ConjuntoIndependente();
-		System.err.println("Solução: "+c.guloso());
-	}
-	
-	
-	public ConjuntoIndependente() {
-		lerEntrada();
-		
-		sGulosa = guloso();
-		
-		calculaGrau();
-		tamMelhor = 0;
-		iniciaSolucaoInicial();
-		
-		branch_and_bound(sInicial, 0);
-		
-		System.err.println(solucao());
+		new ConjuntoIndependente();
 	}
 	
 	public void lerEntrada(){
@@ -62,185 +45,119 @@ public class ConjuntoIndependente {
 			}
 			arq.close();
 			
-			printVerificar();
-			
 		} 
 		catch (FileNotFoundException e) { e.printStackTrace();}
 		catch (IOException e) { e.printStackTrace();}
 
 	}
-	
-	//Algoritmo GULOSO
-	public ArrayList<Integer> guloso(){
-		
-		ArrayList<Integer> solucao = new ArrayList<>();
-		
-		calculaGrau();
-		iniciaPert();
-		
-		while(verificaFim()){
-			
-			//pegar menor grau
-			int vMenorGrau = menorGrau();
-			
-			//add vertice de menor grau
-			solucao.add(vMenorGrau);
-				
-			//pertinecia - vertice
-			vPertinencia.set(vMenorGrau, 1);
-				
-			int i = 0;
-			//pertinecia - vertices vizinhos
-			for(int lVertice : grafo.get(vMenorGrau)){
-				if(lVertice == 1){
-					vPertinencia.set(i, 1);
-				}
-				i++;
-			}
-		}
-		
-		return solucao;
-		
-	}
-	//métodos usados no algoritmo guloso
 	public void calculaGrau(){
 		for(ArrayList<Integer> vertice : grafo){
 			int soma = 0;
 			for(int grau : vertice){
 				soma += grau;
 			}
-			System.err.print(soma+" ");
 			vGraus.add(soma);
 		}
 		System.err.println();
 	}
-	public int menorGrau(){
-		
-		int vertice = vGraus.indexOf(Collections.min(vGraus));
-		vGraus.set(vertice, Integer.MAX_VALUE);
-		
-		if(verificaPert(vertice)){
-			return vertice;
-		}
-		return menorGrau();		
-	}
-	public boolean verificaPert(int vMenorGrau){
-		if(vPertinencia.get(vMenorGrau) == 1)
-			return false;
-		return true;
-	}	
-	public boolean verificaFim(){
-		int soma = 0;
-		for(int v : vPertinencia){
-			soma += v;
-		}
-		if(soma == nVertice)
-			return false;
-		return true;
-	}
-	public void iniciaPert(){
-		for(int i = 0; i < nVertice; i++)
-			vPertinencia.add(0);
-	}
-
-	//métodos usados no algoritmo branch_and_bound
-	public void iniciaSolucaoInicial(){
+	public void solucaoIncial(){
 		for(int i = 0; i < nVertice; i++){
-			vPertBB.add("nUsado");
-			sInicial.add("viavel");
+			solucao.add(-1);
+			inicial.add(0);
 		}
 	}
 	
-	public void branch_and_bound(ArrayList<String> s, int i){
+	public ConjuntoIndependente() {
+		lerEntrada();
+		calculaGrau();
+		qtVerticeSolucao = 0;
+		solucaoIncial();
+		
+		branch_and_bound(inicial, 0);
+		
+		System.err.println(criarSolucao());
+	}
+	
+	public void branch_and_bound(ArrayList<Integer> v, int i){
+		
 		if(eCompleto(i)){
-			vMelhor = s;
-			tamMelhor = qtUsado(s);
+			if(qtVerticesUsados(v) > qtVerticeSolucao){
+				for(int k = 0; k < v.size(); k++)
+					solucao.set(k, v.get(k));
+				qtVerticeSolucao = qtVerticesUsados(v);
+			}
 		}
 		else{
-			s.add(i,"nUsado");
-			if(eConsistente(s,i) && ePromissor(s,i))
-				branch_and_bound(s, i+1);
-			s.add(i, "usado");
-			if(eConsistente(s,i) && ePromissor(s,i))
-				branch_and_bound(s, i+1);
-			s.add(i, "viavel");
+			//v.set(i, -1);
+			if(eConsistente(v, i) && ePromissor(v, i)){
+				branch_and_bound(v, i+1);
+			}
+			v.set(i, 1);
+			if(eConsistente(v, i) && ePromissor(v, i)){
+				branch_and_bound(v, i+1);
+			}
+			v.set(i, 0);
+			
 		}
 	}
 	
-	public boolean eCompleto(int i) {
-	    if(i < vMelhor.size())
-	        return false;
-	    return true;
+	public boolean eCompleto(int i){
+		if(i < solucao.size())
+			return false;
+		return true;
 	}
 	
-	public int qtUsado(ArrayList<String> s) {
-	    int count = 0;
-	    for(int i = 0; i< s.size(); i++)
-	        if(s.get(i).equals("usado"))
-	            count++;
-	    return count;
-	}
-	
-	public boolean eConsistente(ArrayList<String> s, int i) {
-	    if(s.get(i) == "usado")
-	        for(int j = 0; j < i; j++)
-	            if(s.get(j) == "usado" && grafo.get(j).get(i) == 0) {
-	                return false;
-	            }
-	    return true;
-	}
-	
-	public boolean ePromissor(ArrayList<String> s, int i) {
-	    int count = 0;
-	    for(int j = 0; j <= i; j++)
-	        if(s.get(j) == "usado") {
-	            count++;
-	            if(vGraus.get(j) < tamMelhor) {
-	                return false;
-	            }
-	        }
-	    
-	    if((count + (s.size() - i)) <= tamMelhor)
-	        return false;
-	    return true;
-	}
-	
-	public ArrayList<Integer> solucao() {
-		ArrayList<Integer> s = new ArrayList<>();
-	    for(int i = 0; i< vMelhor.size(); i++)
-	        if(vMelhor.get(i) == "usado")
-	            s.add(i+1);
-	    return s;
-	}
-	
-	
-	public void printVerificar(){
-
-		System.err.println("___________________________");
-		
-		//Imprimir numero de Variáveis
-		System.err.println("Numero de vértices: "+getnVertice());
-		
-		//Imprimir Clausulas
-		System.err.println("\nMatriz: ");
-		for(ArrayList<Integer> linha : grafo){
-			for(int i = 0; i < getnVertice(); i++)
-				System.err.print(linha.get(i)+" ");
-			System.err.println();
+	public int qtVerticesUsados(ArrayList<Integer> v){
+		int cont = 0;
+		for(int i = 0; i < v.size(); i++){
+			if(v.get(i) == 1){
+				cont++;
+			}
 		}
-		
-//		//Imprimir Graus
-//		for(int linhaVertice : graus)
-//			System.err.print(linhaVertice+" ");
-		
-		System.err.println("+++++++++++++++++++++++++++\n");
+		return cont;
 	}
-
-	public int getnVertice() {
-		return nVertice;
+	
+	public boolean eConsistente(ArrayList<Integer> v, int i){
+		if(v.get(i) == 1){
+			int j = 0;
+			while(j < i){
+				if(v.get(j) == 1 && (grafo.get(j).get(i) == 1 || grafo.get(i).get(j) == 1)){
+					return false;
+				}
+				j++;
+			}
+		}
+		return true;
 	}
-	public void setnVertice(int nVar) {
-		this.nVertice = nVar;
+	
+	public boolean ePromissor(ArrayList<Integer> v, int j){
+		int i = 0;
+		int consitentes = 0;
+		
+		while(i < nVertice){
+			if(v.get(i) == -1 || v.get(i) == 0){
+				if(eConsistente(v, i)){
+					consitentes++;
+				}
+			}
+			i++;
+		}
+			
+		if((consitentes + qtVerticesUsados(v)) >= qtVerticeSolucao)
+			return true;
+		
+		return false;
+	}
+	
+	public ArrayList<Integer> criarSolucao(){
+		ArrayList<Integer> solFinal = new ArrayList<>();
+		int i = 0;
+		for(int s : solucao){
+			if(s == 1)
+				solFinal.add(i);
+			i++;
+		}
+		return solFinal;
 	}
 
 }
---
+++ src/proj_paa_SAT/Guloso.java
@@ -0,0 +1,159 @@
+package proj_paa_SAT;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Guloso {
+	
+	private int nVertice;
+	private ArrayList<ArrayList<Integer>> grafo = new ArrayList<>();
+	private ArrayList<Integer> vGraus = new ArrayList<>();
+	private ArrayList<Integer> vPertinencia = new ArrayList<>();
+
+	public static void main(String[] args) {
+		Guloso c = new Guloso();
+		System.err.println("Solução: "+c.guloso());
+	}
+	
+	
+	public Guloso() {
+		lerEntrada();
+	}
+	
+	public void lerEntrada(){
+		
+		try {
+			BufferedReader arq = new BufferedReader(new FileReader("conj_indep.txt"));
+			int nLinha = 0;
+			
+			while(arq.ready()){
+				if(nLinha == 0){
+					nVertice = Integer.parseInt(arq.readLine());
+					nLinha++;
+				}
+				else{
+					String[] partes = arq.readLine().split(" ");
+					ArrayList<Integer> temp = new ArrayList<>();
+					for(int i = 0; i < nVertice; i++){
+						temp.add(Integer.parseInt(partes[i]));
+					}
+					grafo.add(temp);
+				}
+			}
+			arq.close();
+			
+			printVerificar();
+			
+		} 
+		catch (FileNotFoundException e) { e.printStackTrace();}
+		catch (IOException e) { e.printStackTrace();}
+
+	}
+	
+	//Algoritmo GULOSO
+	public ArrayList<Integer> guloso(){
+		
+		ArrayList<Integer> solucao = new ArrayList<>();
+		
+		calculaGrau();
+		iniciaPert();
+		
+		while(verificaFim()){
+			
+			//pegar menor grau
+			int vMenorGrau = menorGrau();
+			
+			//add vertice de menor grau
+			solucao.add(vMenorGrau);
+				
+			//pertinecia - vertice
+			vPertinencia.set(vMenorGrau, 1);
+				
+			int i = 0;
+			//pertinecia - vertices vizinhos
+			for(int lVertice : grafo.get(vMenorGrau)){
+				if(lVertice == 1){
+					vPertinencia.set(i, 1);
+				}
+				i++;
+			}
+		}
+		
+		return solucao;
+		
+	}
+	//métodos usados no algoritmo guloso
+	public void calculaGrau(){
+		for(ArrayList<Integer> vertice : grafo){
+			int soma = 0;
+			for(int grau : vertice){
+				soma += grau;
+			}
+			System.err.print(soma+" ");
+			vGraus.add(soma);
+		}
+		System.err.println();
+	}
+	public int menorGrau(){
+		
+		int vertice = vGraus.indexOf(Collections.min(vGraus));
+		vGraus.set(vertice, Integer.MAX_VALUE);
+		
+		if(verificaPert(vertice)){
+			return vertice;
+		}
+		return menorGrau();		
+	}
+	public boolean verificaPert(int vMenorGrau){
+		if(vPertinencia.get(vMenorGrau) == 1)
+			return false;
+		return true;
+	}	
+	public boolean verificaFim(){
+		int soma = 0;
+		for(int v : vPertinencia){
+			soma += v;
+		}
+		if(soma == nVertice)
+			return false;
+		return true;
+	}
+	public void iniciaPert(){
+		for(int i = 0; i < nVertice; i++)
+			vPertinencia.add(0);
+	}
+	
+	public void printVerificar(){
+
+		System.err.println("___________________________");
+		
+		//Imprimir numero de Variáveis
+		System.err.println("Numero de vértices: "+getnVertice());
+		
+		//Imprimir Clausulas
+		System.err.println("\nMatriz: ");
+		for(ArrayList<Integer> linha : grafo){
+			for(int i = 0; i < getnVertice(); i++)
+				System.err.print(linha.get(i)+" ");
+			System.err.println();
+		}
+		
+//		//Imprimir Graus
+//		for(int linhaVertice : graus)
+//			System.err.print(linhaVertice+" ");
+		
+		System.err.println("+++++++++++++++++++++++++++\n");
+	}
+
+	public int getnVertice() {
+		return nVertice;
+	}
+	public void setnVertice(int nVar) {
+		this.nVertice = nVar;
+	}
+
+}
